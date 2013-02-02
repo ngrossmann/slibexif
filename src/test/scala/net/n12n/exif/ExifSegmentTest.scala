d9b9c1289bf87056812c91f4e5a644152dbd5b6c
Niklas Grossmann <ngrossmann@gmx.net>
2013-02-02 13:59:32 +0100

    attr/findAttr methods

---
+++ src/test/scala/net/n12n/exif/ExifSegmentTest.scala
@@ -0,0 +1,22 @@
+package net.n12n.exif
+import org.scalatest.FunSuite
+
+class ExifSegmentTest extends FunSuite {
+  val exif = new JpegMetaData(classOf[ExifSegmentTest].getResourceAsStream("image-gps.jpg")).
+    exif match {
+      case Some(e) => e
+      case None => throw new Exception("Exif segment not found")
+    }
+  
+  test("findAttr finds TiffAttribute") {
+    assert(exif.findAttr(TiffIfd.XResolution) != None, "XResoultion not found in IFD0")
+  }
+  
+  test("findAttr finds ExifAttribute") {
+    assert(exif.findAttr(ExifIfd.ExifVersion) != None, "ExifVersion not found")
+  }
+  
+  test("findAttr finds GpsAttribute") {
+    assert(exif.findAttr(GpsIfd.GPSLongitude) != None, "GPSLongitude not found")
+  }
+}
