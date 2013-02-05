b99f9cf0978f2a9cda751e9d5e5d46c2d832bd79
Niklas Grossmann <ngrossmann@gmx.net>
2013-02-05 23:01:17 +0100

    Changed example

--- examples/src/main/scala/net/n12n/exif/examples/GoogleMaps.scala
+++ examples/src/main/scala/net/n12n/exif/examples/GoogleMaps.scala
@@ -1,15 +1,16 @@
 package net.n12n.exif.examples
 
 import net.n12n.exif._
+import java.io.FileInputStream
 
+/**
+ * Read GPS data from jpeg file and and print URL pointing to the place where the picture was 
+ * taken in Google maps. Call with path to jpeg file as single argument.
+ */
 object GoogleMaps extends App {
-  val exif = new JpegMetaData(this.getClass().getResourceAsStream("image-gps.jpg")).exif match {
-    case Some(exif) => exif
-    case None => throw new Exception("Image does not contain Exif Segment")
-  }
-  
+  require(args.length == 1)
+  val exif = new JpegMetaData(new FileInputStream(args(0))).exif.get  
   val gpsIfd = exif.gpsIfd.get
-  
   val List(latDeg, latMin, latSec) = gpsIfd.value(GpsIfd.GPSLatitude)
   val List(lonDeg, lonMin, lonSec) = gpsIfd.value(GpsIfd.GPSLongitude)
   val latitude = latDeg.toDouble() + latMin.toDouble() / 60 + latSec.toDouble() /3600
