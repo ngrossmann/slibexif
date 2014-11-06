05b7e79c4ba00dd61c33ce201f9a84d313b98c0a
Niklas Grossmann <ngrossmann@gmx.net>
2014-11-06 23:26:59 +0100

    Updated repository information

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 libexif
 =======
 
+[![Build Status](https://travis-ci.org/ngrossmann/slibexif.svg?branch=master)](https://travis-ci.org/ngrossmann/libexif)
+
 Scala library to read JPEG Exif data. Check the wiki to see some examples.
 
 
--- project/Build.scala
+++ project/Build.scala
@@ -1,22 +1,27 @@
 import sbt._
 import Keys._
-import sbtrelease._
-import sbtrelease.ReleasePlugin._
-import sbtrelease.ReleaseStateTransformations._
 
 object LibExifBuild extends Build {
   val dependencies = Seq(
       "org.scalatest" %% "scalatest" % "2.1.7" % "test" cross CrossVersion.binary
   )
-  val mySettings = Defaults.defaultSettings ++ releaseSettings ++ Seq(
+  val mySettings = Defaults.defaultSettings ++ Seq(
       libraryDependencies ++= dependencies,
-      name := "libexif",
+      name := "slibexif",
       organization := "net.n12n.exif",
       scalaVersion in ThisBuild := "2.11.4",
       crossScalaVersions := Seq("2.10.4", "2.11.4"),
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
       testOptions ++= Seq(Tests.Argument("-oSDW")), 
       version in ThisBuild := "0.2.0-SNAPSHOT",
+      credentials += Credentials(Path.userHome / ".sbt" / "sonatype"),
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (isSnapshot.value)
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
       pomExtra := <licenses>
         <license>
           <name>GNU LESSER GENERAL PUBLIC LICENSE, Version 3</name>
@@ -25,25 +30,14 @@ object LibExifBuild extends Build {
         </license>
       </licenses>
       <scm>
-        <url>https://github.com/ngrossmann/libexif</url>
-      </scm>,
-      ReleaseKeys.releaseProcess :=  Seq[ReleaseStep](
-    		  checkSnapshotDependencies,
-    		  inquireVersions, 
-    		  runTest,
-    		  setReleaseVersion, 
-    		  commitReleaseVersion,
-    		  tagRelease,
-    		  // publishArtifacts,
-    		  setNextVersion,
-    		  commitNextVersion,
-    		  pushChanges)
+        <url>https://github.com/ngrossmann/slibexif</url>
+      </scm>
   )
   
-  lazy val libexif = Project(id = "libexif", base = file("."), settings = mySettings)
+  lazy val libexif = Project(id = "slibexif", base = file("."), settings = mySettings)
   
   val exampleSettings = Defaults.defaultSettings ++ Seq(
-      libraryDependencies <+= version("net.n12n.exif" %% "libexif" % _)
+      libraryDependencies <+= version("net.n12n.exif" %% "slibexif" % _)
   )
   lazy val examples = Project(id = "examples", base = file("examples"),
       settings = exampleSettings)
